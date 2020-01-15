be5091a0316938fd7e8df95768ba56c7f756d9da
Stefan Turzer <turzer.stefan@gmail.com>
2020-01-15 02:46:04 +0100

    finally a good icon

--- src/main/java/dev/bakakawaii/PygmalionIcon.java
+++ src/main/java/dev/bakakawaii/PygmalionIcon.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class PygmalionIcon {
-    public static final Icon ICON = IconLoader.findIcon("/icons/crown.png");
+    public static final Icon ICON = IconLoader.findIcon("/icons/mug-hot-solid.svg");
 }
